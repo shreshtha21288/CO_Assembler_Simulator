626ab8767246b25ba32b80459761610de87d729d
grim-pink <108571768+grim-pink@users.noreply.github.com>
2022-08-06 10:26:41 +0530

    Update SimpleSimulator.py

--- SimpleSimulator.py
+++ SimpleSimulator.py
@@ -1,5 +1,20 @@
 import sys 
 # c=input()
+class MEMORY:
+    memory=[]
+    def initialize(self):
+        for line in sys.stdin:
+            self.memory+=[line]
+        if len(self.memory)<=255:
+            empty_space=256-len(self.memory)
+            while empty_space!=0:
+                self.memory+=["0*16"]  
+                empty_space=empty_space-1
+    def getdata(self,prog_count):
+        return self.memory[prog_count]
+    def dump(self):
+        for inst in self.memory:
+            print(inst)
 registers = {
                     "000": 0,               # R0
                     "001": 0,               # R1
