8d3f4556894370ccad9e316d5bb150d8036bd237
grim-pink <108571768+grim-pink@users.noreply.github.com>
2022-08-06 10:48:02 +0530

    Update SimpleSimulator.py

--- SimpleSimulator.py
+++ SimpleSimulator.py
@@ -8,14 +8,14 @@ class MEMORY:
         if len(self.memory)<=255:
             empty_space=256-len(self.memory)
             while empty_space!=0:
-                self.memory+=["0*16"]  
+                self.memory+=["0"*16]  
                 empty_space=empty_space-1
     def getdata(self,prog_count):
         return self.memory[prog_count]
     def dump(self):
         for inst in self.memory:
             print(inst)
-registers = {
+rf = {
                     "000": 0,               # R0
                     "001": 0,               # R1
                     "010": 0,               # R2
@@ -23,7 +23,53 @@ registers = {
                     "100": 0,               # R4
                     "101": 0,               # R5
                     "110": 0,               # R6
+                    "FLAGS":"0"*16
                 }
+
+def execution(inst,rf,pc,var_mem):
+    #inst contains the line,rf is the registers,pc is the program counter
+    #je
+    if inst[:5]=="01111":
+        if rf["FLAGS"][-1]=="1":
+            pc=int(inst[8:],2)
+        else:
+            pc+=1
+        rf["FLAGS"]="0"*16 
+    #jgt
+    if inst[:5]=="01101":
+        if rf["FLAGS"][-2]=="1":
+            pc=int(inst[8:],2)
+        else:
+            pc+=1
+        rf["FLAGS"]="0"*16  
+    #jlt
+    if inst[:5]=="01100":
+        if rf["FLAGS"][-3]=="1":  
+            pc=int(inst[8:],2)
+        else:
+            pc+=1
+        rf["FLAGS"]="0"*16  
+    #jmp
+    if inst[:5]=="11111":
+        pc=int(inst[8:],2)
+        rf["FLAGS"]="0"*16
+    #ld
+    if inst[:5]=="10100":
+        ch=str(int(inst[5:8],2))
+        rf["FLAGS"]="0"*16
+        pc+=1
+        rf["R"+ch]=str[int(inst[8:],2)]
+        var_mem+=str[int(inst[8:],2)]
+        #update the var mem where var_mem is the list containing the memory for the variables
+    #st
+    if inst[:5]=="10101":
+        ch=str(int(inst[5:8]),2)
+        rf["FLAGS"]="0"*16
+        pc+=1
+        if str(int(inst[8:],2)) not in var_mem:
+            var_mem+=str(int(inst[8:],2))
+        i=var_mem.index(int(inst[8:],2))
+        var_mem[i]=rf["R"+ch]
 def add(self):
     r1=int(self[:3],2)
     r2=int(self[3:7],2)
